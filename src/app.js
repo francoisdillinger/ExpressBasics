e930fa0bafc703cfcb17fe053ba099ed549a3c41
James Jester <elmojester@gmail.com>
2016-06-17 21:21:51 -0500

    app code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 'use strict';
 
-var express = require('express');
-
+var express = require('express'),
+    posts = require('./fakedata/posts.json');
+var postLists = Object.keys(posts).map(function(value){
+    return posts[value]});
 var app = express();
+// use() defines middleware, which is logic that tells express how to handle a request between the time a request is made by a client but before it arrives at a route
+app.use('/static',express.static( __dirname + '/public'));
 
-
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/templates');
+//debugger;
 app.get('/', function(req, res){
-    res.send('<h1>How is this?</h1>');
+    res.render('index');
 });
-
+app.get('/blog/:title?', function(req, res){
+    var title = req.params.title;
+    if(title === undefined){
+        res.status(503);
+        res.render('blog', {posts: postLists});
+    }
+    else{
+       var post = posts[title] || {};
+       res.render('post', {post: post}); 
+    }
+    
+});
 app.listen(3000, function() {
     console.log('The frontend server is on port 3000 you genius!');
 });
